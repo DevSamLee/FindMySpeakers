4456a6ac8e0d085cd4e4fc8e6dab9b10a6b2a61f
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-02-24 18:49:58 -0600

    Update data.js

--- start/js/data.js
+++ start/js/data.js
@@ -2,9 +2,9 @@ const qnaList = [
   {
     q: '1. When I am tired, I will choose...?  ',
     a: [
-      { answer: 'a. sleep.', type: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] },
-      { answer: 'b. hang out with people', type: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] },
-      { answer: 'c. eating.', type: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] },
+      { answer: 'a. sleep.', type: [0, 1, 2, 3, 4, 5, 6, 7, 8] },
+      { answer: 'b. hang out with people', type: [0, 1, 2, 3, 4, 5, 6, 7, 8] },
+      { answer: 'c. eating.', type: [0, 1, 2, 3, 4, 5, 6, 7, 8] },
     ]
   },
   {
@@ -12,7 +12,7 @@ const qnaList = [
     a: [
       { answer: 'a. Technical/industry insight', type: [1, 2, 4] },
       { answer: 'b. Softskills/people-skills', type: [0, 3, 6, 5] },
-      { answer: 'c. Problem solving/success stories', type: [7, 8, 9] },
+      { answer: 'c. Problem solving/success stories', type: [7, 8] },
     ]
   },
   {
@@ -20,7 +20,7 @@ const qnaList = [
     a: [
       { answer: 'a. No, we need focus on people on earth.', type: [0, 1, 5] },
       { answer: 'b. Absolutely, we should support space projects for future generation.', type: [3, 6, 5, 7] },
-      { answer: 'c. I am not sure, maybe?', type: [2, 4, 8, 9] },
+      { answer: 'c. I am not sure, maybe?', type: [2, 4, 8] },
     ]
   },
   {
@@ -28,22 +28,22 @@ const qnaList = [
     a: [
       { answer: 'a. Move fast, break things', type: [0, 1, 2, 6] },
       { answer: 'b. Slow and steady wins the race', type: [3, 4, 5] },
-      { answer: 'c. Depends on what the boss says', type: [7, 8, 9] }
+      { answer: 'c. Depends on what the boss says', type: [7, 8] }
     ]
   },
   {
     q: '5. Where do you prefer to store your data such as images and videos from traveling?',
     a: [
-      { answer: 'a. External hard drive" ', type: [1, 2, 4] },
-      { answer: 'b. Cloud" ', type: [7, 8, 9] },
-      { answer: 'c. Printing" ', type: [0, 3, 6, 5] },
+      { answer: 'a. External hard drive', type: [1, 2, 4] },
+      { answer: 'b. Cloud', type: [7, 8] },
+      { answer: 'c. Printing', type: [0, 3, 6, 5] },
     ]
   },
 
   {
     q: '6. If you need some advice which way do you prefer?',
     a: [
-      { answer: 'a. Meet mentor or collegues', type: [3, 6, 7, 8, 9] },
+      { answer: 'a. Meet mentor or collegues', type: [3, 6, 7, 8] },
       { answer: 'b. Reseaching', type: [1, 4, 5] },
       { answer: 'c. Leave it for now, I need to refresh myelf', type: [2] },
     ]
@@ -53,13 +53,13 @@ const qnaList = [
     a: [
       { answer: 'a. Use my lucky number like birthday, aniversary combination', type: [3, 6, 7, 8] },
       { answer: 'b. Random generator!', type: [1, 4, 5] },
-      { answer: 'c. I will buy the lottery ticket at store that had serveral winners!', type: [0, 2, 9] },
+      { answer: 'c. I will buy the lottery ticket at store that had serveral winners!', type: [0, 2] },
     ]
   },
   {
     q: '8. If you worked at a big company, where would you be?',
     a: [
-      { answer: 'a. Where the Customers are.', type: [0, 1, 4, 7, 9] },
+      { answer: 'a. Where the Customers are.', type: [0, 1, 4, 7] },
       { answer: 'b. Where my boss is.', type: [3, 8] },
       { answer: 'c. Where the servers are.', type: [2, 5, 6] },
     ]
@@ -68,7 +68,7 @@ const qnaList = [
     q: '9. After you graduate, what will you be inspired to do?',
     a: [
       { answer: 'a. Work with a great team and build things.', type: [4, 5] },
-      { answer: 'b. Start your own venture.', type: [0, 1, 7, 8, 9] },
+      { answer: 'b. Start your own venture.', type: [0, 1, 7, 8] },
       { answer: 'c. Move somewhere warmer.', type: [2, 3, 6] },
     ]
   },
@@ -82,6 +82,10 @@ const infoList = [
   {
     name: 'Your result is Emerging Technology.',
     desc: ' '
+  },
+  {
+    name: 'Your result is Data Science and Machine Learning.',
+    desc: ''
   },
   {
     name: 'Your result is Information Security.',
